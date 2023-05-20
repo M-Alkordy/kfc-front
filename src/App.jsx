691d7a44e9488d8da136b22e4ef5494a3f659633
jaber ibrahim <jaber.ibrahim0937@gmail.com>
2023-05-20 04:16:59 +0300

    slider

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Carousel from './Carousel/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='nav'>navbar</h1>
+      <Carousel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Carousel/CarouselData.js
@@ -0,0 +1,26 @@
+export const CarouselData = [
+    {
+        id          :1,
+        title       : "one Lorem ipsum dolor sit amet et perferendis",
+        description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+        img         : "https://placehold.jp/852x537.png"
+    },
+    {
+        id          :2,
+        title       : "two Lorem ipsum dolor sit amet et perferendis",
+        description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+        img         : "https://placehold.jp/852x537.png"
+    },
+    {
+        id          :3,
+        title       : "three Lorem ipsum dolor sit amet et perferendis",
+        description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+        img         : "https://placehold.jp/852x537.png"
+    },
+    {
+        id          :4,
+        title       : "four Lorem ipsum dolor sit amet et perferendis",
+        description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+        img         : "https://placehold.jp/852x537.png"
+    },
+];

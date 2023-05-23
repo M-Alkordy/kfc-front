8c54e4377c4e6db3d10c1aca08badcb4f10fadae
jaber ibrahim <jaber.ibrahim0937@gmail.com>
2023-05-23 14:51:37 +0300

    about page by Jaber and Fatimah

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import About from './About/About';
 import './App.css';
-import Carousel from './Carousel/Carousel';
+// import Carousel from './Carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='nav'>navbar</h1>
-      <Carousel/>
+      
+      <About/>
     </div>
   );
 }
---
+++ src/CarouselAbout/CarouselAboutData.js
@@ -0,0 +1,100 @@
+// export const CarouselData = [
+//     {
+//         id          :1,
+//         title       : "one Lorem ipsum dolor sit amet et perferendis",
+//         description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+//         img         : "https://placehold.jp/852x537.png"
+//     },
+//     {
+//         id          :2,
+//         title       : "two Lorem ipsum dolor sit amet et perferendis",
+//         description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+//         img         : "https://placehold.jp/852x537.png"
+//     },
+//     {
+//         id          :3,
+//         title       : "three Lorem ipsum dolor sit amet et perferendis",
+//         description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+//         img         : "https://placehold.jp/852x537.png"
+//     },
+//     {
+//         id          :4,
+//         title       : "four Lorem ipsum dolor sit amet et perferendis",
+//         description : "Lorem ipsum dolor sit amet. Et perferendis vero qui doloremque sint ea dolores odit sit consectetur velit ex doloremque omnis. Qui architecto maxime qui nulla eveni",
+//         img         : "https://placehold.jp/852x537.png"
+//     },
+// ];
+
+import {images} from "../assests/images.jsx"
+export const CarouselStepData = [
+    {
+        id   :  1,
+        step : "step1",
+        title : "inspect",
+        description : "lorem1 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id.",
+        img : images[3].url,
+    },
+    {
+        id   :  2,
+        step : "step2",
+        title : "inspect",
+        description : "lorem2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  3,
+        step : "step3",
+        title : "inspect",
+        description : "lorem3 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id.",
+        img : images[3].url,
+    },
+    {
+        id   :  4,
+        step : "step4",
+        title : "inspect",
+        description : "lorem4 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  5,
+        step : "step5",
+        title : "inspect",
+        description : "lorem5Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  6,
+        step : "step6",
+        title : "inspect",
+        description : "lorem6 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  7,
+        step : "step7",
+        title : "inspect",
+        description : "lorem7 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  8,
+        step : "step8",
+        title : "inspect",
+        description : "lorem8 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id.",
+        img : images[3].url,
+    },
+    {
+        id   :  9,
+        step : "step9",
+        title : "inspect",
+        description : "lorem9lorem8 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+    {
+        id   :  10,
+        step : "step10",
+        title : "inspect",
+        description : "lorem10lorem8 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias vitae ipsa consequuntur fugit non unde, nostrum suscipit laboriosam inventore perferendis fugiat voluptate maxime maiores molestiae corrupti, minima fuga. Minima, id. ",
+        img : images[3].url,
+    },
+]

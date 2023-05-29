5140170773b8bfa2e251d62ba7dc2dfa238658ef
hashem othman <hashim993.hel993@emitisgroup.com>
2023-05-29 12:42:17 +0200

    done

--- src/Footer.js
+++ src/Footer.js
@@ -1,23 +1,27 @@
 import './Footer.css'
-import React from 'react';
+import React, { useState } from 'react';
 import FooterSecond from './FooterSecond';
 
 
 function Footer() {
+  const [onClick, setOnClick] =useState ('false')  ;
     return (
      <div className="footer">
         <div className='container'>
           <div className='footer-margin'>
-            <img src='#' className="footer-logo" alt="logo" />
+            <div className='logo'>
+            <h1  className="footer-logo"  >Kentucky Fried Chicken</h1>
+            </div>
+              
             <div className='footer-search '>
               <p className='text-white search-text'>Find a KFC</p>
-              <input type="text" class="search" placeholder='Search By city and state or Zip code' />
+              <input type="text" class="search" placeholder='Search By city and state or Zip code'  />
               </div>
               <div className='icons'> 
-              <img src='#' className="svg" alt="" />
-              <img src='#' className="svg" alt="" />
-              <img src='#' className="svg" alt="" />
-            </div>
+                <div className='svg'> </div>
+                <div className='svg'> </div>
+                <div className='svg'> </div>
+             </div>
 
           </div>
 

fba41c23098833799dc2384bad07c961ff134a22
fatimasouliman <fffaat.sss@gmail.com>
2023-05-30 22:04:45 +0300

    added filter to allmeals page

--- src/componants/Allmeals.jsx
+++ src/componants/Allmeals.jsx
@@ -4,11 +4,16 @@ import '../assest/style/allproducts.css';
 import les from '../assest/images/Les-W5-22-AllinOne-websitebanner-1536x481-01 (1) 1.jpg';
 import taste from '../assest/images/KFC-W3-22-Taste-Gaurentee-Website-Banner-1536x481-02 (1) 1.jpg';
 import { IoIosArrowForward } from 'react-icons/io';
-function Allmeals({data}) {
-    const titlecombos = "Combos";
-    const titlenuggets = "Nuggets";
-    const titlesandwiches = "Sandwiches";
-    const titleoffers = "Special Offers";
+import { useState } from 'react';
+function Allmeals() {
+    const meals = [
+        "Special Offers",
+        "Combos",
+        "Nuggets",
+        "Sandwiches",
+        
+    ] ;
+    const [newmeals , setNewmeals] = useState(meals);
     const sides = [
         {
             title: "12 PC Tenders",
@@ -24,6 +29,14 @@ function Allmeals({data}) {
         },
        
     ]
+    const mealsfilter = (event) =>{
+        
+            const Newmeals  = meals.filter(meal => meal === event.target.innerHTML);
+            setNewmeals(Newmeals);
+    
+      
+    }
+    
     return (
         <div className='w-100 all'>
             <div className='face'>
@@ -35,19 +48,21 @@ function Allmeals({data}) {
             </div>
             
             <ul className='filter mt-4'>
-                <li className='select'>Special Offers</li>
-                <li>Combos</li>
-                <li>Nuggets</li>
-                <li>Sandwiches</li>
-                <li>Tenders</li>
-                <li>Bowls & Pot Pies</li>
+                <li onClick={(event)=> mealsfilter(event)} >Special Offers</li>
+                <li onClick={(event)=> mealsfilter(event)} >Combos</li>
+                <li onClick={(event)=> mealsfilter(event)}>Nuggets</li>
+                <li onClick={(event)=> mealsfilter(event)}>Sandwiches</li>
+                <li onClick={(event)=> mealsfilter(event)}>Tenders</li>
+                <li onClick={(event)=> mealsfilter(event)}>Bowls & Pot Pies</li>
                 <li><IoIosArrowForward /></li>
             </ul>
-            <div>
-                <Cardproducts data={sides} titlehead={titleoffers} />
-                <Cardproducts data={sides} titlehead={titlecombos} />
-                <Cardproducts data={sides} titlehead={titlenuggets} />
-                <Cardproducts data={sides} titlehead={titlesandwiches} />
+            <div >
+                {
+                    newmeals.map(meal =>
+                    <Cardproducts data={sides} titlehead={meal} />
+                    )
+                }
+                
             </div>
             
             <img className='w-100 mt-5' src={les}/>
